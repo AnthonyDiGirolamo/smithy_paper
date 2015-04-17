f52dd5f3faab3ec73127bf8be61613854e2c94f9
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-04-17 14:46:55 -0400

    cleaned up the flow for formula section, added some formula helpers subsections

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-pdflatex -shell-escape sc_paper
-bibtex sc_paper
-pdflatex -shell-escape sc_paper
-pdflatex -shell-escape sc_paper
+pdflatex -shell-escape sc_paper && bibtex sc_paper && pdflatex -shell-escape sc_paper && pdflatex -shell-escape sc_paper
